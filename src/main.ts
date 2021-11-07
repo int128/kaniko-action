db09cb00fa2979b4e7eafd98476aaf9a76dc0c51
Hidetake Iwata <int128@gmail.com>
2021-11-07 15:58:13 +0900

    Initial commit (#2)

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,19 @@ import * as core from '@actions/core'
 import { run } from './run'
 
 const main = async (): Promise<void> => {
-  await run({
-    name: core.getInput('name', { required: true }),
+  const outputs = await run({
+    executor: core.getInput('executor', { required: true }),
+    kanikoArgs: core.getMultilineInput('kaniko-args'),
+    buildArgs: core.getMultilineInput('build-args'),
+    context: core.getInput('context', { required: true }),
+    file: core.getInput('file', { required: true }),
+    labels: core.getMultilineInput('labels'),
+    noCache: core.getBooleanInput('no-cache'),
+    push: core.getBooleanInput('push'),
+    tags: core.getMultilineInput('tags'),
+    target: core.getInput('target'),
   })
+  core.setOutput('digest', outputs.digest)
 }
 
 main().catch((e) => core.setFailed(e instanceof Error ? e.message : JSON.stringify(e)))
--- tests/run.test.ts
+++ tests/run.test.ts
@@ -1,5 +1,93 @@
-import { run } from '../src/run'
+import * as os from 'os'
+import { generateArgs } from '../src/run'
 
-test('run successfully', async () => {
-  await expect(run({ name: 'foo' })).resolves.toBeUndefined()
+test('default args', () => {
+  const args = generateArgs(
+    {
+      executor: 'gcr.io/kaniko-project/executor:latest',
+      kanikoArgs: [],
+      buildArgs: [],
+      context: '.',
+      file: 'Dockerfile',
+      labels: [],
+      noCache: false,
+      push: false,
+      tags: [],
+      target: '',
+    },
+    '/tmp/kaniko-action'
+  )
+  expect(args).toStrictEqual([
+    // docker args
+    'run',
+    '--rm',
+    '-v',
+    `${process.cwd()}:/workspace:ro`,
+    '-v',
+    `/tmp/kaniko-action:/output`,
+    '-v',
+    `${os.homedir()}/.docker/config.json:/kaniko/.docker/config.json:ro`,
+    'gcr.io/kaniko-project/executor:latest',
+    // kaniko args
+    '--context',
+    'dir:///workspace/',
+    '--dockerfile',
+    'Dockerfile',
+    '--digest-file',
+    '/output/digest',
+    '--no-push',
+  ])
+})
+
+test('full args', () => {
+  const args = generateArgs(
+    {
+      executor: 'gcr.io/kaniko-project/executor:latest',
+      kanikoArgs: ['--verbosity=debug'],
+      buildArgs: ['foo=1', 'bar=2'],
+      context: '.',
+      file: 'Dockerfile',
+      labels: ['org.opencontainers.image.description=foo', 'org.opencontainers.image.url=https://www.example.com'],
+      noCache: false,
+      push: false,
+      tags: ['helloworld:latest', 'ghcr.io/int128/kaniko-action/example:v1.0.0'],
+      target: 'server',
+    },
+    '/tmp/kaniko-action'
+  )
+  expect(args).toStrictEqual([
+    // docker args
+    'run',
+    '--rm',
+    '-v',
+    `${process.cwd()}:/workspace:ro`,
+    '-v',
+    `/tmp/kaniko-action:/output`,
+    '-v',
+    `${os.homedir()}/.docker/config.json:/kaniko/.docker/config.json:ro`,
+    'gcr.io/kaniko-project/executor:latest',
+    // kaniko args
+    '--context',
+    'dir:///workspace/',
+    '--dockerfile',
+    'Dockerfile',
+    '--digest-file',
+    '/output/digest',
+    '--build-arg',
+    'foo=1',
+    '--build-arg',
+    'bar=2',
+    '--label',
+    'org.opencontainers.image.description=foo',
+    '--label',
+    'org.opencontainers.image.url=https://www.example.com',
+    '--no-push',
+    '--destination',
+    'helloworld:latest',
+    '--destination',
+    'ghcr.io/int128/kaniko-action/example:v1.0.0',
+    '--target',
+    'server',
+    '--verbosity=debug',
+  ])
 })
