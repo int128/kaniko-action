fd960f5feb77df13aab14e16cf52edeb868bfe9c
Hidetake Iwata <int128@gmail.com>
2023-08-14 14:23:21 +0900

    Show stacktrace on error

--- src/main.ts
+++ src/main.ts
@@ -22,4 +22,7 @@ const main = async (): Promise<void> => {
   core.setOutput('digest', outputs.digest)
 }
 
-main().catch((e) => core.setFailed(e instanceof Error ? e : String(e)))
+main().catch((e: Error) => {
+  core.setFailed(e)
+  console.error(e)
+})
